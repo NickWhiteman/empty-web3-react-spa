265fb5e4769858dcd6eaec1709ed26dc22361110
Nick Whiteman <romanov_nikita@hotmail.com>
2023-03-09 05:26:04 +0300

    build default project state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useConnect } from 'wagmi';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { ExampleService } from './services/example-service/src';
 
 const App = () => {
     const { connect } = useConnect({
         connector: new MetaMaskConnector(),
     });
-	
-    return <div className="App"></div>;
+
+    return (
+        <div className="App">
+            <ExampleService />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/services/example-service/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+export const ExampleService = () => (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+reportWebVitals();
